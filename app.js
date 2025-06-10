3e03bc86355f4c178b5d07b40992dc8a06146341
Edson Medeiros da Silva Pezuti Junior <edson.mspjunior@senacsp.edu.br>
2025-06-09 21:17:07 -0300

    Conexão com database

--- app.js
+++ app.js
@@ -2,9 +2,47 @@ const express = require('express');
 
 const app = express();
 
+const mysql = require('mysql2');
+ 
+const conexao = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'senac',
+  port: 3306,
+  database: 'db_pi_keyloan'
+});
+
 app.get("/", function(req, res){
     res.write("Hello World!");
     res.end();
 });
 
+conexao.connect((erro) => {
+  if (erro) {
+    console.error('😫 Erro ao conectar ao banco de dados:', erro);
+    return;
+  }
+  console.log('😁 Conexão com o banco de dados estabelecida com sucesso!');
+});
+
 app.listen(8080);
+
+/*
+const mysql = require('mysql2');
+ 
+const conexao = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'senac',
+  port: 3306,
+  database: 'ecommerce_pc'
+});
+ 
+conexao.connect((erro) => {
+  if (erro) {
+    console.error('😫 Erro ao conectar ao banco de dados:', erro);
+    return;
+  }
+  console.log('😁 Conexão com o banco de dados estabelecida com sucesso!');
+});
+*/
